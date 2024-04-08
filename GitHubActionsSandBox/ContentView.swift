6a91aa09db1f2a40213615cf2aaed2ae025e05df
eguchi-ap <t.eguchi@alphapolis.co.jp>
2024-04-08 10:28:24 +0900

    Initial Commit

---
+++ GitHubActionsSandBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHubActionsSandBox
+//
+//  Created by WEBSYSTEM-MAC39 on 2024/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHubActionsSandBox/GitHubActionsSandBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsSandBoxApp.swift
+//  GitHubActionsSandBox
+//
+//  Created by WEBSYSTEM-MAC39 on 2024/04/08.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsSandBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
